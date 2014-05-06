176312e03e4a194a7f7b47bd4c76f0a32b2b86b3
Migue Martin <miguegt@hotmail.es>
2014-05-06 18:56:51 +0200

    Autenticacion y acceso a gestion usuarios

---
+++ index.py
@@ -0,0 +1,32 @@
+from bottle import route, request, run, view, template
+from passlib.apps import custom_app_context as pwd_context
+import pymongo
+from pymongo import Connection
+import datetime
+import os
+
+
+@route('/')
+def login():
+    return '''
+        <form action="/login" method="post">
+            Username: <input name="username" type="text" />
+            Password: <input name="password" type="password"/>
+            <input value="Login" type="submit" />
+        </form>
+    '''
+
+@route('/login', method='POST')
+@view('form_template')
+def do_login():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+    hash = '$6$rounds=42278$ZbbYfgk1TSSeHS96$KVLiVfN9Zq02lEdqbwVDyosijXj0o72C/VPMOneIYpzD4lt.PjOo7HRS3s442UjeNX/3T.f/5T1TJOcXSyyR.1'
+    ok = pwd_context.verify(password, hash)
+    if username == "admin" and ok == True:
+		return template('template') 
+    else:
+		return "Necesita ser adminitrador para acceder"
+
+run(host='localhost', port=8020)
+
